3f45869f7acd97d9de3c03612d831a969813aecd
wny <wny1990@gmail.com>
2014-07-17 21:33:55 -0500

    134/151

--- wildcard_matching.cpp
+++ wildcard_matching.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-
-bool match(const char *s, int i, const char *p,int j )
-{
-	if (i >= len1 || j >= len2 )
-		return false;
-	char p1 = p[j];
-	if( p1  == '\0')
-	{
-		if (s[i] == '\0')
-			return true;
-		else
-			return false;
-	}
-	if (p1 == '?')
-		return match(s,i+1,p,j+1);
-	if (p[j] == '*')	
-		return ( match(s,i,p,j+1) || match(s,i,p,j+1) || match(s,i+1,p,j+1) );
-	else 
-	{
-		if (s[i] == p[j])
-			return  match(s,i+1,p,j+1) ;
-	 	return false;
-	}
-	return false;
-}
 bool isMatch(const char *s, const char *p) 
 {
+	int len1,len2;
 	for( len1 = 0; s[len1]!='\0';len1++);
 	for( len2 = 0; p[len2]!='\0';len2++);
-	return match(s,0,p,0);       
+	vector<vector<bool> > flag(len1+2,vector<bool>(len2+2,false));
+	flag[0][0] = true;
+	for( int i = 1; i <= len1 ; i++);
+		
+	for( int i = 1; i <= len1 ; i++);
+		for( int j = 1; j <= len2 ; j++);
+		{
+			
+		}
+	return flag[len1][len2];
 }
-private:
-	int len1,len2;
 };
